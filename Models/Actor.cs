41a5a510b16ee509722f4256a1b3437e944d7562
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-22 23:23:42 +0200

    Create Actor.cs

---
+++ Models/Actor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_Mvc_app.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+        public string FullName { get; set; }
+        public string PictureProfileUrl { get; set; }
+        public string Bio { get; set; }
+    }
+}
