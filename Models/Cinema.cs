8106457ab24b5367913cc75d04d7dde4432bc5f2
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-23 09:35:34 +0200

    Create Cinema.cs

---
+++ Models/Cinema.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_Mvc_app.Models
+{
+    public class Cinema
+    {
+        [Key]
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
