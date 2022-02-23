fd22a1f19994cd8effa043fe41dc9d78e9b6f2f7
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-02-23 09:25:39 +0200

    Update Producer.cs

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -1,8 +1,13 @@
-﻿namespace E_commerce_Mvc_app.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerce_Mvc_app.Models
 {
     public class Producer
     {
+        [Key]
+        public int ProducerId { get; set; }
         public string FullName{ get; set; }
         public string PictureProfileUrl { get; set; }
+        public string Bio { get; set; }
     }
 }
