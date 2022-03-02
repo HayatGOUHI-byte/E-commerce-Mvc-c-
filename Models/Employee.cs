72703b0eec4398c4fb039d473860e95152c5e171
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-02 15:14:03 +0200

    update

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,6 +1,17 @@
-﻿namespace E_commerce_Mvc_app.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DB_Context.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+
+        public Department Department { get; set; }
     }
 }
