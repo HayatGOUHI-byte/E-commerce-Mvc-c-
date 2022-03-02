068ffb4a73e6f4298d7fbd44fca479cc4338f759
h120ayat <93711127+girlcoder3@users.noreply.github.com>
2022-03-02 15:48:36 +0200

    dotnet ef migrations add Migration1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,5 +28,7 @@ namespace E_commerce_Mvc_app.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+     
     }
 }
